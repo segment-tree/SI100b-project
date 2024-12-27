3383fd87ce748bceefb31b6805d9baa8c398ed4d
Blind-Guess-Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2024-12-27 21:55:28 +0800

    first change temporary commit 2

--- New demo/collections.py
+++ New demo/collections.py
@@ -15,6 +15,7 @@ Core
 
 import typing as _typing
 import sys as _sys
+from multiprocessing.managers import EventProxy
 
 import pygame as _pygame
 
@@ -68,7 +69,7 @@ class EventLike:
         event: pygame.event.Event
             pygame事件
         """
-        a = cls(event.type, sender="pygame", prior=10) # 利用pygame事件创建EventLike实例
+        a = cls(event.type, EventSender="pygame", EventPrior=10) # 利用pygame事件创建EventLike实例
         a.__dict__.update(event.__dict__) # 继承属性 得到一个字典
         return a
 
@@ -83,5 +84,52 @@ class EventLike:
             距离上次UPDATE事件经过的时间
         """
         body: _const.UpdateEventBody = {"second": second}
-        return cls(_const.Event)
+        return cls(_const.EventCode.UPDATE, EventBody=body, EventPrior=20)
+
+    def __init__(
+        self,
+        EventCode: int,
+        *,
+        EventPrior: int = 10,
+        EventSender: str = "",
+        EventReceivers: _typing.Set[str] = None,
+        EventBody: _typing.Optional[_typing.Dict[str, _typing.Any]] = None,
+    ) -> None:
+        """
+
+        :param EventCode:
+            事件代码 独一无二的
+        :param EventPrior:
+            优先级
+        :param EventSender:
+            事件发送者（UUID）
+        :param EventReceivers:
+            事件目标接收者（UUID集合） 默认参数提供一个空集合，接收者添加入其中
+        :param EventBody:
+            事件附加信息
+        """
+        assert isinstance(EventCode, int)
+        assert isinstance(EventPrior, int)
+        assert isinstance(EventSender, str)
+        assert isinstance(EventReceivers, (set, None.__class__))
+        assert isinstance(EventBody, (dict, None.__class__))
+        self.EventCode: int = EventCode
+        self.EventPrior: int = EventPrior
+        self.EventSender: str = EventSender
+        self.EventReceivers: _typing.Set[str] = (
+                EventReceivers if EventReceivers is not None else {_const.EVERYONE_RECEIVER}
+        )
+
+    def __lt__(self, other: "EventLike") -> bool:
+        """
+        运算符重载: `<`, 根据`prior`进行比较。
+        """
+        return self.EventPrior < other.EventPrior
+    def __gt__(self, other: "EventLike") -> bool:
+        """
+        运算符重载: `>`, 根据`prior`进行比较。
+        """
+        return self.EventPrior > other.EventPrior
+
+
 
