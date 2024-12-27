a6f572b1ab344158a9fa1ab53eadbc21e7dd5023
Blind-Guess-Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2024-12-27 22:51:54 +0800

    first change temporary commit 4

--- New demo/collections.py
+++ New demo/collections.py
@@ -15,7 +15,6 @@ Core
 
 import typing as _typing
 import sys as _sys
-from multiprocessing.managers import EventProxy
 
 import pygame as _pygame
 
@@ -55,6 +54,7 @@ class EventLike:
     receiver: _typing.Set[str]
     prior: int
     body: _typing.Dict[str, _typing.Any]
+    eventType: str
 
     key: _typing.Optional[int]
 
@@ -69,7 +69,7 @@ class EventLike:
         event: pygame.event.Event
             pygame事件
         """
-        a = cls(event.type, EventSender="pygame", EventPrior=10) # 利用pygame事件创建EventLike实例
+        a = cls(event.type, sender="pygame", prior=10) # 利用pygame事件创建EventLike实例
         a.__dict__.update(event.__dict__) # 继承属性 得到一个字典
         return a
 
@@ -84,40 +84,78 @@ class EventLike:
             距离上次UPDATE事件经过的时间
         """
         body: _const.UpdateEventBody = {"second": second}
-        return cls(_const.EventCode.UPDATE, EventBody=body, EventPrior=20)
+        body.type = "UPDATE"
+        return cls(_const.EventCode.UPDATE, body=body, prior=20)
+
+    @classmethod
+    def kill_event(cls, uuid: str) -> "EventLike":
+        """
+        创建KILL事件
+
+        ---
+        :param uuid: str
+            被删除监听者的UUID
+        :return: object
+            一个初始化好的EventLike对象
+        """
+        body: _const.KillEventBody = {"suicide": uuid}
+        body.type = "KILL"
+        return cls(_const.EventCode.KILL, body=body, sender=uuid, prior=0)
+
+    @classmethod
+    def draw_event(
+            cls,
+            window: _pygame.Surface,
+            *,
+            receivers: _typing.Set[str] = None,
+            camera: _typing.Tuple[int, int] = (0, 0)
+    ):
+        """
+
+        :param window: pygame.Surface
+            一般是pygame.display.set_mode(...)返回的Surface对象, 占满整个窗口的画布
+        :param receivers: set[str], typing.Optional, default = {EVERYONE_RECEIVER}
+            事件接收者, 默认是任何Listener
+        :param camera: tuple[int, int], default = (0, 0)
+            相机位置, 绘制偏移量
+        :return: object
+            一个初始化好的EventLike对象
+        """
+        body: _const.DrawEventBody = {"window": window, "camera": camera}
+        body.type = "DRAW"
+        return cls(_const.EventCode.DRAW, body=body, prior=300, receivers=receivers)
 
     def __init__(
         self,
         EventCode: int,
         *,
-        EventPrior: int = 10,
-        EventSender: str = "",
-        EventReceivers: _typing.Set[str] = None,
-        EventBody: _typing.Optional[_typing.Dict[str, _typing.Any]] = None,
+        prior: int = 10,
+        sender: str = "",
+        receivers: _typing.Set[str] = None,
+        body: _typing.Optional[_typing.Dict[str, _typing.Any]] = None,
     ) -> None:
         """
-
         :param EventCode:
             事件代码 独一无二的
-        :param EventPrior:
+        :param prior:
             优先级
-        :param EventSender:
+        :param sender:
             事件发送者（UUID）
-        :param EventReceivers:
+        :param receivers:
             事件目标接收者（UUID集合） 默认参数提供一个空集合，接收者添加入其中
-        :param EventBody:
+        :param body:
             事件附加信息
         """
         assert isinstance(EventCode, int)
-        assert isinstance(EventPrior, int)
-        assert isinstance(EventSender, str)
-        assert isinstance(EventReceivers, (set, None.__class__))
-        assert isinstance(EventBody, (dict, None.__class__))
+        assert isinstance(prior, int)
+        assert isinstance(sender, str)
+        assert isinstance(receivers, (set, None.__class__))
+        assert isinstance(body, (dict, None.__class__))
         self.EventCode: int = EventCode
-        self.EventPrior: int = EventPrior
-        self.EventSender: str = EventSender
+        self.EventPrior: int = prior
+        self.EventSender: str = sender
         self.EventReceivers: _typing.Set[str] = (
-                EventReceivers if EventReceivers is not None else {_const.EVERYONE_RECEIVER}
+                receivers if receivers is not None else {_const.EVERYONE_RECEIVER}
         )
 
     def __lt__(self, other: "EventLike") -> bool:
@@ -131,11 +169,24 @@ class EventLike:
         """
         return self.EventPrior > other.EventPrior
 
+
+PostEventAPILike: _typing.TypeAlias = _typing.Callable[
+    [EventLike], None
+]  # 事件发布函数类型注释, 一般使用`Core`的`add_event`函数
 class ListenerLike:
     """
     监听者
 
     1. ‘self.listen(event)’函数监听事件
-        (1) 首先检查事件类型和接收者集
+        (1) 首先检查事件类型和接收者集(`EventLike.code`, `EventLike.receivers`)
+        (2) 根据事件类型`EventLike.code`, 将事件传递到对应的被`listening`(basetools模块提供)装饰过的函数。
+    2. `self.post(event)`函数可以发布事件 (发布位置取决初始化时传入的`post_api`, 推荐使用`Core().add_event`作为`post_api`)
+
+    :param
     """
+    __post_api: _typing.Optional[PostEventAPILike]
+    __listen_receivers: _typing.Set[str]
+    __listen_methods: _typing.Dict[
+        int, _typing.Set[_typing.Callable[[EventLike], None]]
+    ]
 
