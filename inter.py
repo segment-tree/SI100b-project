1199fd55bb2721536bf46e2cb1481a3e89166e70
segment-tree <ruizema@yeah.net>
2025-01-06 11:38:14 +0800

    fix

--- inter.py
+++ inter.py
@@ -11,7 +11,7 @@ class player(creature):
     readToInteract:bool
     def keyboard(self, keys:pygame.key.ScancodeWrapper): # 捕捉键盘信息
         allowF=thisMap.moveRequest
-        if c.alwaysAllow:allowF=lambda _a,_b,_c : True
+        if c.alwaysAllow:allowF=lambda x,y,entity : True
         #python有for-else语句但没有 elfor 有什么让这段代码美观的方案吗？？
         for i in c.KeyboardLeft:
             if keys[i]: self.tryMove(-1,0,allowF);break
